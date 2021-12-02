f21284cdc46e55a8ca6b7a3017b25a8762a0e15b
Jordan Yee <jordanyee799@gmail.com>
2021-12-02 11:37:37 -0800

    finished

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,16 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        self.freebies.map{|free| free.item_name == item_name}.include?(true)
+    end
+
+    def give_away(dev, freebie)
+        if (self.received_one?(freebie.item_name))
+            freebie.update({dev_id: dev.id})
+        else
+            "dev doesn't have this item"
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+puts "Clearing old data..."
+Company.destroy_all
+Dev.destroy_all
+Freebie.destroy_all
+
 puts "Creating companies..."
 Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
@@ -17,5 +22,9 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+Freebie.create(item_name: "stickers", value: 10, company_id: Company.ids.sample, dev_id: Dev.ids.sample)
+Freebie.create(item_name: "bag", value: 20, company_id: Company.ids.sample, dev_id: Dev.ids.sample)
+Freebie.create(item_name: "water bottle", value: 15, company_id: Company.ids.sample, dev_id: Dev.ids.sample)
+Freebie.create(item_name: "pen", value: 5, company_id: Company.ids.sample, dev_id: Dev.ids.sample)
 
 puts "Seeding done!"
